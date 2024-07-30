ad9bb1e7e1d28d8064ebd2f00e03ad5dbcb4bd72
LeoninZemlyanykh <zemlyanykh@bk.ru>
2024-07-30 11:26:21 +0500

    Выполнение заданий

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.marks) {
+    this.marks.push(...marks);
+  } else {
+    console.log("Студент отчислен");
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks && this.marks.length) {
+    let sum = this.marks.reduce((acc, item) => acc + item, 0)
+    return sum / this.marks.length
+  } else {
+    return 0
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
