4132875b5700e97052fee3c278e52ee219088b6b
LeoninZemlyanykh <zemlyanykh@bk.ru>
2024-08-02 14:16:54 +0500

    Выполнение задания

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(value) {
+    if (isNaN(value)) {
+        throw new Error("Невалидное значение")
+    }
+    return Number.parseFloat(value);
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error
+    }
+    
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        const sqrt = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return Number(sqrt.toFixed(3))
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a,b,c)
+    } catch(error) {
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует"
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует"
+            }
+        }
+    }
+}
